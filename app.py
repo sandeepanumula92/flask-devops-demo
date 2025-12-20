cc9f8d2658f6e571e6858c539abb1e896dfd517d
Sandeep Anumula <sandeep.anumula@outlook.com>
2025-12-20 12:54:58 +0000

    Integrate Prometheus metrics into Flask app
    Added Prometheus metrics for request count and latency.

--- app.py
+++ app.py
@@ -1,14 +1,44 @@
 from flask import Flask
+from prometheus_client import Counter, Histogram, generate_latest
+import time
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter(
+    "http_requests_total",
+    "Total HTTP requests",
+    ["method", "endpoint"]
+)
+
+REQUEST_LATENCY = Histogram(
+    "http_request_latency_seconds",
+    "HTTP request latency",
+    ["endpoint"]
+)
+
 @app.route("/")
 def hello():
-    return "Hello from Flask running in Kubernetes!"
+    REQUEST_COUNT.labels("GET", "/").inc()
+    return "Hello from Flask running in kubernetes (kind) with Metrics!"
 
 @app.route("/health")
 def health():
+    REQUEST_COUNT.labels("GET", "/health").inc()
     return {"status": "ok"}
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest()
+
+@app.before_request
+def before_request():
+    app.start_time = time.time()
+
+@app.after_request
+def after_request(response):
+    latency = time.time() - app.start_time
+    REQUEST_LATENCY.labels(endpoint="/").observe(latency)
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
